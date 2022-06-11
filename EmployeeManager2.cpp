67ff982a71e50ebb87c5017141a8800d224cc0df
Eyal <romrom63@gmail.com>
2022-06-11 19:36:58 +0300

    finished functions for rank tree before testing

--- EmployeeManager2.cpp
+++ EmployeeManager2.cpp
@@ -6,50 +6,94 @@
 
 Company *EmployeeManager::getCompany(int n)
 {
+    int to_return = 0;
+    if (n== 0)
+    {
+        return this->companyArray[0];
+    }
+   if (n > this->size)
+   {
+       return nullptr;
+   }
+   to_return = this->companyUF->find(n);
+    return  this->companyArray[to_return];
 
 }
 
 Employee *EmployeeManager::getEmployee(int id)
 {
-
+    auto* newEmployee = new Employee(Id,0,0);
+    auto* checkIfHere = companyArray[0]->findEmployee(newEmployee);
+    if (checkIfHere == nullptr)
+    {
+        delete newEmployee;
+        return nullptr;
+    }
+    delete newEmployee;
+    Employee* data = checkIfHere;
+    return data;
 }
 
-int EmployeeManager::AddEmployee(int employeeID, int companyID, int grade)
+
+StatusType EmployeeManager::AddEmployee(int employeeID, int companyID, int grade)
 {
 
 }
 
-int EmployeeManager::RemoveEmployee(int employeeID)
+StatusType EmployeeManager::RemoveEmployee(int employeeID)
 {
 
 }
 
-int EmployeeManager::PromoteEmployee(int employeeID, int bumpGrade)
+StatusType EmployeeManager::PromoteEmployee(int employeeID, int bumpGrade)
+{
+    if (employeeID < =0)
+    {
+        return INVALID_INPUT;
+    }
+    Employee* req_employee = getEmployee(employeeID);
+    if (req_employee == nullptr)
+    {
+        return FAILURE;
+    }
+    req_employee->BumpGrade(bumpGrade);
+    companyArray[req_employee->getCompany()]->employeeGradeWasChanged(req_employee);
+    return SUCCESS;
+
+}
+
+StatusType EmployeeManager::EmployeeSalaryIncrease(int employeeID, int salaryIncrease)
+{
+    if (employeeID < =0)
+    {
+        return INVALID_INPUT;
+    }
+    Employee* req_employee = getEmployee(employeeID);
+    if (req_employee == nullptr)
+    {
+        return FAILURE;
+    }
+    req_employee->IncreaseSalary(salaryIncrease);
+    companyArray[req_employee->getCompany()]->employeeSalaryChanged(req_employee);
+    return SUCCESS;
+}
+
+StatusType EmployeeManager::AcquireCompany(int companyID1, int companyID2, double factor)
 {
 
 }
 
-int EmployeeManager::EmployeeSalaryIncrease(int employeeID, int salaryIncrease)
+StatusType EmployeeManager::SumOfBumpGradeBetweenTopWorkersByGroup(int companyID, int m)
 {
 
 }
 
-int EmployeeManager::AcquireCompany(int companyID1, int companyID2, double factor)
+StatusType EmployeeManager::AverageBumpGradeBetweenSalaryByGroup(int companyID, int lowerSalary, int higherSalary)
 {
 
 }
 
-int EmployeeManager::SumOfBumpGradeBetweenTopWorkersByGroup(int companyID, int m)
-{
-
-}
-
-int EmployeeManager::AverageBumpGradeBetweenSalaryByGroup(int companyID, int lowerSalary, int higherSalary)
-{
-
-}
-
-int EmployeeManager::CompanyValue(int companyID)
+StatusType EmployeeManager::CompanyValue(int companyID)
 {
 
 }
